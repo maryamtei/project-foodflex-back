42080b3c32a299c4aa767bd86781b5ff3ec656aa
TEI Maryam <tei.mary.dev@gmail.com>
2023-07-25 22:00:29 +0200

    ajout validation schema pour contact

--- app/routers/routerContact.js
+++ app/routers/routerContact.js
@@ -7,7 +7,7 @@ const contactController = require('../controllers/contactController');
 /*------------ Validation_Schema ---------------- */
 
 const validator = require('../validation/validator'); // Schema validator
-// const validateSubmitFormSchema = require('../validation/');
+const validateSubmitFormSchema = require('../validation/contactSchema/submitFormSchema');
 
 
 /*------------ Middlewares ---------------- */
@@ -18,7 +18,7 @@ const controllerWrapper = require('../middlewares/controllerWrapper');
 
 const router = express.Router();
 
-router.post(`/contact`, /*validator('body', validateSubmitFormSchema)*/ controllerWrapper(contactController.submitContactForm));
+router.post(`/contact`, validator('body', validateSubmitFormSchema), controllerWrapper(contactController.submitContactForm));
 
 
 module.exports = router;
